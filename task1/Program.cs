aaeda87d541824cd489c620f7cfa7cdf59164cd0
diplg <dimas4354@icloud.com>
2022-05-26 21:32:43 +0300

    сортировка массива по строкам

---
+++ task1/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int rows = 4;
+int cols = 4;
+int[,] matrix = new int[rows, cols];
+
+FillMatrix();
+Console.WriteLine("Дан массив: ");
+PrintMatrix();
+Console.WriteLine();
+Sort();
+Console.WriteLine("Отсортированный по строкам массив: ");
+PrintMatrix();
+
+void FillMatrix()
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+void PrintMatrix()
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Sort()
+{
+    int temp = 0;
+    // int k = 0;
+    // int l = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, j] < matrix[i, k])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+
+    }
+}
